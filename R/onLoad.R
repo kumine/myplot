ae9af3d1989dfca17a84f6aeda0f29b567b180f0
caumine <caumine@gmail.com>
2019-03-09 18:07:15 +0800

    V1.0

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,8 +1,9 @@
-# .onLoad <- function(libname, pkgname){
-# cat("
-#   
-# )
-# }
-# 
+.onLoad <- function(libname, pkgname){
+cat("
+See more information: https://github.com/kumine/myplot
+Chinese:  https://www.jianshu.com/p/e2219188f14e
+    ")
+}
+
 
 
