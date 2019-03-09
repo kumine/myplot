6dbec017bad161b37d6788c904689713f209d253
caumine <caumine@gmail.com>
2019-03-09 17:50:40 +0800

    V1.0

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,6 @@
 # .onLoad <- function(libname, pkgname){
 # cat("
-#   welcome to use Ropt package
-#   Powerful Parsing Command-Line Arguments and Variable Interpolation
-#   it's very useful to write pipeline by R.
-#   by caumine@gmai.com.\n"
+#   
 # )
 # }
 # 
